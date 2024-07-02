5fd2b33b9fdda3e6be9fd3758d36ca5feb31648a
LeonardoNevesAeroespacial <leonardo.neves@aeroespacial.eng.br>
2024-07-01 21:46:46 -0300

    Save chnages

--- main.cpp
+++ main.cpp
@@ -12,99 +12,19 @@
 #include <thread>
 #include <atomic>
 #include <iomanip>
+#include <exception>
 
-#include "searchAlgorithm.h"
-
-#include "linearSearch.h"
+#include "AVLTree.h"
 #include "binarySearch.h"
 #include "binarySearchTree.h"
-#include "AVLTree.h"
+#include "linearSearch.h"
 #include "redBlackTree.h"
+#include "treapTree.h"
 
 #include "datasetGenerator.h"
 
-using namespace std;
+#include "ProgressBar.hpp"
 
-std::mutex mtx;
-
-// Este método é responsável em executar um algoritmo, medir o tempo de CPU time e gravar os resultados de movimentações, comparações e tempo de execução em um arquivo CSV
-void runExperiment(int iteration, const std::string& functionName, SearchAlgorithm* method, const std::vector<int>& dataset, const std::string& datasetName, std::ofstream& file) {
-
-    // Executando o método insere, mensurando o tempo de execução e contando comparações
-    int counter_comparisons_insere = 0;
-    std::clock_t start = std::clock();
-    auto result_testInsere = method->testInsere(dataset, &counter_comparisons_insere);
-    std::clock_t end = std::clock();
-    long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-
-    // Executando o método pesquisa, mensurando o tempo de execução e contando comparações
-    int counter_comparisons_pesquisa = 0;
-    start = std::clock();
-    auto result_testPesquisa = method->testPesquisa(result_testInsere, dataset, &counter_comparisons_pesquisa);
-    end = std::clock();
-    long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-
-    // Executando o método retira, mensurando o tempo de execução e contando comparações
-    int counter_comparisons_retira = 0;
-    start = std::clock();
-    auto result_testRetira = method->testRetira(result_testInsere, dataset, &counter_comparisons_retira);
-    end = std::clock();
-    long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
-
-    // Registrando resultados no arquivo CSV
-    {
-        std::lock_guard<std::mutex> lock(mtx);
-        file << iteration + 1 << ";" << functionName << ";" << datasetName << ";" 
-             << dataset.size() << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
-             << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
-             << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
-    }
-}
-
-// Este método é responsável por executar um interação do código, o que envolve executar um conjunto de dados sobre uma série de métodos utilizando computação paralela
-void runInteration(std::map<std::string, SearchAlgorithm*> methods, int iteration, std::vector<std::vector<std::vector<int>>> datasets, std::vector<std::string> datasets_name, std::string output_path, std::string posfixo = "") {
-
-    // Interando sobre cada método
-    for (auto function = methods.begin(); function != methods.end(); ++function) {
-
-        // Obtendo tempo atual
-        auto now = std::chrono::system_clock::now();
-        std::time_t now_time = std::chrono::system_clock::to_time_t(now);
-        std::tm* now_tm = std::localtime(&now_time);
-
-        // Criando arquivo CSV de saída
-        char time_str[100];
-        std::strftime(time_str, sizeof(time_str), "%Y-%m-%d_%H-%M-%S", now_tm);
-        std::string filename = output_path + "/output_" + function->first + "_" + std::string(time_str) + "_" + std::to_string(iteration) + "_" + posfixo + ".csv";
-        std::ofstream file(filename);
-
-        // Criando threads para executar os algoritmos em paralelo
-        if (file.is_open()) {
-            file << "Iteration;Algorithm;DatasetName;DatasetSize;Time Insere;Counter Comparisons Insere;Time Pesquisa;Counter Comparisons Pesquisa;Time Retira;Counter Comparisons Retira\n";
-
-            for (int i = 0; i < datasets.size(); ++i) {
-
-                std::vector<std::thread> threads;
-
-                
-
-
-                for (int j = 0; j < datasets[i].size(); ++j) {
-                    runExperiment(iteration, function->first, std::move(function->second), datasets[i][j], datasets_name[i], std::ref(file));
-                    // threads.emplace_back(runExperiment, iteration, function->first, std::move(function->second), datasets[i][j], datasets_name[i], std::ref(file));
-                }
-
-                // for (int i = 0; i < threads.size(); ++i) {
-                //     threads[i].join();
-                // }
-            }
-        }
-
-        file.close();
-    }
-}
-
-// Este método é responsável por gerar um conjunto de dados baseado em um vetor de entrada, que em cada posição do vetor, um número indicando o comprimento do vetor que será gerado
 std::vector<std::vector<std::vector<int>>> generateDataset(std::vector<int> lengthLists) {
 
     /*
@@ -173,45 +93,81 @@ std::vector<std::vector<std::vector<int>>> generateDataset(std::vector<int> leng
     return datasets;
 }
 
-int main()
-{
-    // Configurações Iniciais
-    int NUMBER_INTERATIONS = 1; // Número de interações que o código vai executar
-    std::string output_path = "/home/leo/AAED_P2/output"; // Caminho de saída do resultado
+int main() {
 
-    // Configuração dos métodos que será utilizado em cada interação
-    std::map<std::string, SearchAlgorithm*> methods;
-    // methods["linearSearch"] = new LinearSearch();
-    // methods["binarySearch"] = new BinarySearch();
-    // methods["binarySearchTree"] = new BinarySearchTree();
-    methods["avlTree"] = new AVLTree();
-    // methods["redBlackTree"] = new RedBlackTree();
+    std::string output_path = "/home/leo/AAED_P2/output";
 
-    // Nome dos conjuntos de dados, informação que será utilizada posteriormente como identificador no arquivo CSV de resultado
+    std::vector<int> lengthLists = {10, 100, 1000, 10000, 100000, 1000000};
+    
     std::vector<std::string> datasets_name = {
         "Ordered",
         "OrderedInverse",
         "AlmostOrdered",
         "Random"
     };
+    
+    auto datasets = generateDataset(lengthLists);
+    
+    std::map<std::string, SearchAlgorithm*> methods;
+    methods["linearSearch"] = new LinearSearch();
+    methods["binarySearch"] = new BinarySearch();
+    methods["binarySearchTree"] = new BinarySearchTree();
+    methods["avlTree"] = new AVLTree();
+    methods["redBlackTree"] = new RedBlackTree();
+    methods["treapTree"] = new Treap();
 
-    // Executando as interações
-    for (int i = 0; i < NUMBER_INTERATIONS; ++i) {
+    std::ofstream file("output.csv");
 
-        // Gerando um dataset para está interação, executando primeiro para vetores do tamanho, 10, 100, 1.000 e 10.000
-        std::vector<int> lengthLists1 = {10, 100, 1000, 10000};
-        std::vector<std::vector<std::vector<int>>> datasets1 = generateDataset(lengthLists1);
+    file << "Algorithm;DatasetName;DatasetSize;Time Insere;Counter Comparisons Insere;Time Pesquisa;Counter Comparisons Pesquisa;Time Retira;Counter Comparisons Retira\n";
 
-        // Executando a primeira parte da interação
-        runInteration(methods, i, datasets1, datasets_name, output_path, "1");
+    int total_iterations = methods.size() * datasets.size() * datasets[0].size();
+    ProgressBar progress(total_iterations);
+    
+    if (file.is_open()) {
 
-        // // Gerando um dataset para está interação, executando primeiro para vetores do tamanho de 100.000
-        // std::vector<int> lengthLists2 = {100000};
-        // std::vector<std::vector<std::vector<int>>> datasets2 = generateDataset(lengthLists2);
+        for (auto function = methods.begin(); function != methods.end(); ++function) {
+        
+            for (int i = 0; i < datasets.size(); ++i) {
 
-        // // Executando a segunda parte da interação
-        // runInteration(methods, i, datasets2, datasets_name, output_path, "2");
+                for (int j = 0; j < datasets[i].size(); ++j) {
+                    
+                    try {
+                        std::cout << function->first << " " << datasets[i][j].size() << std::endl;
+                        int counter_comparisons_insere = 0;
+                        std::clock_t start = std::clock();
+                        auto result_testInsere = function->second->testInsere(datasets[i][j], &counter_comparisons_insere);
+                        std::clock_t end = std::clock();
+                        long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+                        
+                        int counter_comparisons_pesquisa = 0;
+                        start = std::clock();
+                        auto result_testPesquisa = function->second->testPesquisa(result_testInsere, datasets[i][j], &counter_comparisons_pesquisa);
+                        end = std::clock();
+                        long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+                        
+                        int counter_comparisons_retira = 0;
+                        start = std::clock();
+                        auto result_testRetira = function->second->testRetira(result_testInsere, datasets[i][j], &counter_comparisons_retira);
+                        end = std::clock();
+                        long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+                        
+                        {
+                            file << function->first << ";" << datasets_name[i] << ";" 
+                                << datasets[i][j].size() << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
+                                << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
+                                << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
+                        }
+
+                        progress.update();
+                    } catch (const std::exception& e) {
+                        std::cerr << e.what() << std::endl;
+                    }
+                }
+            }
+        }
     }
+
+    file.close();
 
     return 0;
 }
--- main_treap_dataset.cpp
+++ main_treap_dataset.cpp
@@ -29,91 +29,262 @@ using namespace std;
 int main() {
 
     DatasetGenerator datasetGenerator;
-
-    // std::vector<int> tamanhos = {10, 100, 1000};
     std::vector<int> tamanhos = {10, 100, 1000, 10000, 100000, 1000000};
     std::vector<std::vector<int>> datasets;
-    for (int tam : tamanhos) {
-        int counter = 0; // Reset the counter for each tree size
-        datasets.clear(); // Limpa o vetor de datasets para o próximo tamanho
-        datasets.push_back(datasetGenerator.generateOrdered(tam));
-        datasets.push_back(datasetGenerator.generateOrderedInverse(tam));
-        datasets.push_back(datasetGenerator.generateAlmostOrdered(tam));
-        datasets.push_back(datasetGenerator.generateRandom(tam));
 
-        for (auto dataset: datasets) {
-        
-            std::cout << "Treap" << std::endl;
-            {
-                counter = 0; // Reset the counter for each tree size
-                Treap treap;
-                // std::cout << "1" << std::endl;
-                auto result = treap.testInsere(dataset, &counter);
-                // std::cout << "2" << std::endl;
-                treap.testPesquisa(result, dataset, &counter);
-                // std::cout << "3" << std::endl;
-                treap.testRetira(result, dataset, &counter);
-            }
+    int NUMBER_INTERATIONS = 10;
 
-            std::cout << "AVLTree" << std::endl;
-            {
-                counter = 0; // Reset the counter for each tree size
-                AVLTree avl;
-                // std::cout << "1" << std::endl;
-                auto result = avl.testInsere(dataset, &counter);
-                // std::cout << "2" << std::endl;
-                avl.testPesquisa(result, dataset, &counter);
-                // std::cout << "3" << std::endl;
-                avl.testRetira(result, dataset, &counter);
-            }
+    std::vector<std::string> datasets_name = {
+        "Ordered",
+        "OrderedInverse",
+        "AlmostOrdered",
+        "Random"
+    };
 
-            std::cout << "RedBlackTree" << std::endl;
-            {
-                counter = 0; // Reset the counter for each tree size
-                RedBlackTree redBlack;
-                std::cout << "1" << std::endl;
-                auto result = redBlack.testInsere(dataset, &counter);
-                std::cout << "2" << std::endl;
-                redBlack.testPesquisa(result, dataset, &counter);
-                std::cout << "3" << std::endl;
-                redBlack.testRetira(result, dataset, &counter);
-            }
+    std::vector<std::string> algorithms = {
+        "TreapTree",
+        "AVLTree",
+        "RedBlackTree",
+        "BinarySearchTree",
+        "BinarySearch",
+        "LinearSearch"
+    };
 
-            std::cout << "LinearSearch" << std::endl;
-            {
-                counter = 0; // Reset the counter for each tree size
-                LinearSearch linear;
-                // std::cout << "1" << std::endl;
-                auto result = linear.testInsere(dataset, &counter);
-                // std::cout << "2" << std::endl;
-                linear.testPesquisa(result, dataset, &counter);
-                // std::cout << "3" << std::endl;
-                linear.testRetira(result, dataset, &counter);
-            }
+    for (int j = 0; j < datasets.size(); ++j) {
 
-            std::cout << "BinarySearch" << std::endl;
-            {
-                counter = 0; // Reset the counter for each tree size
-                BinarySearch binarySearch;
-                // std::cout << "1" << std::endl;
-                auto result = binarySearch.testInsere(dataset, &counter);
-                // std::cout << "2" << std::endl;
-                binarySearch.testPesquisa(result, dataset, &counter);
-                // std::cout << "3" << std::endl;
-                binarySearch.testRetira(result, dataset, &counter);
-            }
+        std::ofstream file(std::to_string(j) + ".csv");
 
-            std::cout << "BinarySearchTree" << std::endl;
-            {
-                BinarySearchTree binarySearchTree;
-                // std::cout << "1" << std::endl;
-                auto result = binarySearchTree.testInsere(dataset, &counter);
-                // std::cout << "2" << std::endl;
-                binarySearchTree.testPesquisa(result, dataset, &counter);
-                // std::cout << "3" << std::endl;
-                binarySearchTree.testRetira(result, dataset, &counter);
+        file << "Algorithm;DatasetName;DatasetSize;Time Insere;Counter Comparisons Insere;Time Pesquisa;Counter Comparisons Pesquisa;Time Retira;Counter Comparisons Retira\n";
+
+        if (file.is_open()) {
+
+            for (int tam : tamanhos) {
+
+                std::cout << "Tamanho: " << tam << std::endl;
+
+                int counter = 0;
+                datasets.clear();
+                datasets.push_back(datasetGenerator.generateOrdered(tam));
+                datasets.push_back(datasetGenerator.generateOrderedInverse(tam));
+                datasets.push_back(datasetGenerator.generateAlmostOrdered(tam));
+                datasets.push_back(datasetGenerator.generateRandom(tam));
+
+                for (int i = 0; i < datasets.size(); ++i) {
+                
+                    std::cout << "Treap" << std::endl;
+                    {
+                        int counter_comparisons_insere = 0; // Reset the counter for each tree size
+                        Treap treap;
+                        std::cout << "1" << std::endl;
+                        std::clock_t start = std::clock();
+                        auto result = treap.testInsere(datasets[i], &counter_comparisons_insere);
+                        std::clock_t end = std::clock();
+
+                        long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "2" << std::endl;
+                        int counter_comparisons_pesquisa = 0;
+                        start = std::clock();
+                        treap.testPesquisa(result, datasets[i], &counter_comparisons_pesquisa);
+                        end = std::clock();
+
+                        long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "3" << std::endl;
+                        int counter_comparisons_retira = 0;
+                        start = std::clock();
+                        treap.testRetira(result, datasets[i], &counter_comparisons_retira);
+                        end = std::clock();
+
+                        long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        {
+                            file << algorithms[0] << ";" << datasets_name[i] << ";" 
+                                << tam << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
+                                << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
+                                << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
+                        }
+                    }
+
+                    std::cout << "AVLTree" << std::endl;
+                    {
+                        int counter_comparisons_insere = 0; // Reset the counter for each tree size
+                        AVLTree avlTree;
+                        std::cout << "1" << std::endl;
+                        std::clock_t start = std::clock();
+                        auto result = avlTree.testInsere(datasets[i], &counter_comparisons_insere);
+                        std::clock_t end = std::clock();
+
+                        long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "2" << std::endl;
+                        int counter_comparisons_pesquisa = 0;
+                        start = std::clock();
+                        avlTree.testPesquisa(result, datasets[i], &counter_comparisons_pesquisa);
+                        end = std::clock();
+
+                        long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "3" << std::endl;
+                        int counter_comparisons_retira = 0;
+                        start = std::clock();
+                        avlTree.testRetira(result, datasets[i], &counter_comparisons_retira);
+                        end = std::clock();
+
+                        long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        {
+                            file << algorithms[1] << ";" << datasets_name[i] << ";" 
+                                << tam << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
+                                << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
+                                << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
+                        }
+                    }
+
+                    std::cout << "RedBlackTree" << std::endl;
+                    {
+                        int counter_comparisons_insere = 0; // Reset the counter for each tree size
+                        RedBlackTree redBlackTree;
+                        std::cout << "1" << std::endl;
+                        std::clock_t start = std::clock();
+                        auto result = redBlackTree.testInsere(datasets[i], &counter_comparisons_insere);
+                        std::clock_t end = std::clock();
+
+                        long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "2" << std::endl;
+                        int counter_comparisons_pesquisa = 0;
+                        start = std::clock();
+                        redBlackTree.testPesquisa(result, datasets[i], &counter_comparisons_pesquisa);
+                        end = std::clock();
+
+                        long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "3" << std::endl;
+                        int counter_comparisons_retira = 0;
+                        start = std::clock();
+                        redBlackTree.testRetira(result, datasets[i], &counter_comparisons_retira);
+                        end = std::clock();
+
+                        long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        {
+                            file << algorithms[2] << ";" << datasets_name[i] << ";" 
+                                << tam << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
+                                << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
+                                << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
+                        }
+                    }
+
+                    std::cout << "LinearSearch" << std::endl;
+                    {
+                        int counter_comparisons_insere = 0; // Reset the counter for each tree size
+                        LinearSearch linearSearch;
+                        std::cout << "1" << std::endl;
+                        std::clock_t start = std::clock();
+                        auto result = linearSearch.testInsere(datasets[i], &counter_comparisons_insere);
+                        std::clock_t end = std::clock();
+
+                        long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "2" << std::endl;
+                        int counter_comparisons_pesquisa = 0;
+                        start = std::clock();
+                        linearSearch.testPesquisa(result, datasets[i], &counter_comparisons_pesquisa);
+                        end = std::clock();
+
+                        long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "3" << std::endl;
+                        int counter_comparisons_retira = 0;
+                        start = std::clock();
+                        linearSearch.testRetira(result, datasets[i], &counter_comparisons_retira);
+                        end = std::clock();
+
+                        long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        {
+                            file << algorithms[3] << ";" << datasets_name[i] << ";" 
+                                << tam << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
+                                << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
+                                << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
+                        }
+                    }
+
+                    std::cout << "BinarySearch" << std::endl;
+                    {
+                        int counter_comparisons_insere = 0; // Reset the counter for each tree size
+                        BinarySearch binarySearch;
+                        std::cout << "1" << std::endl;
+                        std::clock_t start = std::clock();
+                        auto result = binarySearch.testInsere(datasets[i], &counter_comparisons_insere);
+                        std::clock_t end = std::clock();
+
+                        long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "2" << std::endl;
+                        int counter_comparisons_pesquisa = 0;
+                        start = std::clock();
+                        binarySearch.testPesquisa(result, datasets[i], &counter_comparisons_pesquisa);
+                        end = std::clock();
+
+                        long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "3" << std::endl;
+                        int counter_comparisons_retira = 0;
+                        start = std::clock();
+                        binarySearch.testRetira(result, datasets[i], &counter_comparisons_retira);
+                        end = std::clock();
+
+                        long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        {
+                            file << algorithms[4] << ";" << datasets_name[i] << ";" 
+                                << tam << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
+                                << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
+                                << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
+                        }
+                    }
+
+                    std::cout << "BinarySearchTree" << std::endl;
+                    {
+                        int counter_comparisons_insere = 0; // Reset the counter for each tree size
+                        BinarySearchTree binarySearchTree;
+                        std::cout << "1" << std::endl;
+                        std::clock_t start = std::clock();
+                        auto result = binarySearchTree.testInsere(datasets[i], &counter_comparisons_insere);
+                        std::clock_t end = std::clock();
+
+                        long double diff_insertion = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "2" << std::endl;
+                        int counter_comparisons_pesquisa = 0;
+                        start = std::clock();
+                        binarySearchTree.testPesquisa(result, datasets[i], &counter_comparisons_pesquisa);
+                        end = std::clock();
+
+                        long double diff_pesquisa = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        std::cout << "3" << std::endl;
+                        int counter_comparisons_retira = 0;
+                        start = std::clock();
+                        binarySearchTree.testRetira(result, datasets[i], &counter_comparisons_retira);
+                        end = std::clock();
+
+                        long double diff_retira = 1000.0 * (end - start) / CLOCKS_PER_SEC;
+
+                        {
+                            file << algorithms[5] << ";" << datasets_name[i] << ";" 
+                                << tam << ";" << diff_insertion << ";" << counter_comparisons_insere<< ";" 
+                                << diff_pesquisa << ";" << counter_comparisons_pesquisa<< ";" 
+                                << diff_retira << ";" << counter_comparisons_retira<< ";" << "\n";
+                        }
+                    }
+                }
             }
         }
+
+        file.close();
     }
 
     return 0;
